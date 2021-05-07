58d3e1487b93f2e2ddbc8393830816946393f4c4
Bradley <b@handley.io>
2021-05-07 07:22:04 -0500

    Working with token and basic auth.

--- web/app.js
+++ web/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello dummy!'));
+app.get('/55jsyeks', (req, res) => res.send('Token auth!'));
+app.get('/', (req, res) => res.send('Password auth!'));
 app.listen(3000, () => console.log('Server ready'));
